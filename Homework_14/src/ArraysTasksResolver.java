1e2cd5326a8469f1b6bc5c44698e69250e6d31af
Osman <avarets1234@gmail.com>
2022-10-16 16:02:56 +0300

    add Homework_14

--- Homework_14/src/ArraysTasksResolver.java
+++ Homework_14/src/ArraysTasksResolver.java
@@ -1,5 +1,32 @@
+import java.util.Arrays;
+
 public class ArraysTasksResolver {
     public static void resolveTask(int[] array, ArrayTask task, int from, int to) {
 
+        System.out.println(Arrays.toString(array));
+        ArraysTasksResolver.printFromTo(array, from, to);
+        int res = task.resolve(array, from, to);
+        System.out.println(res);
+
+
     }
+
+    private static void printFromTo(int[] arr, int from, int to) {
+
+        if (from > to || from < 0 || to > arr.length) {
+            System.out.println("Некорректные данные");
+
+        } else {
+
+            int[] newArr = new int[to - from];
+            int index = 0;
+
+            for (int i = from ; i < to; i++) {
+                newArr[index++] = arr[i];
+            }
+            System.out.println(Arrays.toString(newArr));
+        }
+    }
+
+
 }
