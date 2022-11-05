d98ce632909b2c992262175a2393851e9f2fa6b7
Osman <avarets1234@gmail.com>
2022-11-05 12:39:20 +0300

    refactor IntermediateAttestation

--- IntermediateAttestation/src/Main.java
+++ IntermediateAttestation/src/Main.java
@@ -1,5 +1,8 @@
+import java.util.List;
 import product.Product;
 import product.ProductRepository;
+import product.Tests;
+
 
 public class Main {
   public static void main(String[] args) {
@@ -7,10 +10,30 @@ public class Main {
     ProductRepository productRepository = new ProductRepository("products.txt");
 
     Product bread = productRepository.findById(3);
-    bread.setCount(100);
-    bread.setPrice(25);
+    Tests.assertEquals("Хлеб", bread.getName());
+    Tests.assertEquals(25.0, bread.getPrice());
 
+
+    bread.setPrice(30);
+    bread.setCount(57);
     productRepository.update(bread);
+
+    Product updatedBread = productRepository.findById(3);
+    Tests.assertEquals(30.0, updatedBread.getPrice());
+    Tests.assertEquals(57, updatedBread.getCount());
+
+    testByTitle(productRepository, "М");
+
+
+  }
+
+
+
+  public static void testByTitle(ProductRepository productRepository, String title) {
+    List<Product> products = productRepository.findByTitleLike(title);
+    for (Product prod : products) {
+      Tests.assertEquals(prod.getName(), title);
+    }
 
   }
 }
